01006e52f2fe0a76695551aad0f62c441b2456b1
Denis Cornehl <denis.cornehl@gmail.com>
2020-12-09 12:54:23 +0100

    [d07] some refactoring with lazy-static, iterators and references

--- d07/src/main.rs
+++ d07/src/main.rs
@@ -1,4 +1,6 @@
-use lazy_static::lazy_static;
+#[macro_use]
+extern crate lazy_static;
+
 use regex::Regex;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
@@ -7,20 +9,22 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let file = File::open("input.txt").unwrap();
 
-    println!(
-        "{:?}",
-        doit2(
-            BufReader::new(file).lines().map(|line| line.unwrap()),
-            "shiny gold".to_string()
-        )
-    );
+    let input: Vec<String> = BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap())
+        .collect();
+
+    println!("part 1: {:?}", part_1(input.iter(), "shiny gold"));
+    println!("part 2: {:?}", part_2(input.iter(), "shiny gold"));
 }
 
 lazy_static! {
     static ref REGEX_CONTENT: Regex = Regex::new(r"(\d+) (\w+ \w+) (bag|bags)\.?").unwrap();
 }
 
-fn read_mapping(lines: impl Iterator<Item = String>) -> HashMap<String, HashMap<String, usize>> {
+fn read_mapping<'a>(
+    lines: impl Iterator<Item = &'a String>,
+) -> HashMap<String, HashMap<String, usize>> {
     let mut mapping: HashMap<String, HashMap<String, usize>> = HashMap::new();
 
     for line in lines {
@@ -45,11 +49,11 @@ fn read_mapping(lines: impl Iterator<Item = String>) -> HashMap<String, HashMap<
 fn search_for_colors(
     mapping: &HashMap<String, HashMap<String, usize>>,
     result: &mut HashSet<String>,
-    search_for: String,
+    search_for: &str,
 ) {
     let contains_color: HashSet<String> = mapping
         .iter()
-        .filter(|(_, v)| v.contains_key(&search_for))
+        .filter(|(_, v)| v.contains_key(search_for))
         .map(|(k, _)| k.clone())
         .collect();
 
@@ -57,12 +61,12 @@ fn search_for_colors(
 
     if !(contains_color.is_empty()) {
         for parent_color in contains_color.iter() {
-            search_for_colors(mapping, result, parent_color.clone());
+            search_for_colors(mapping, result, parent_color);
         }
     }
 }
 
-pub fn doit(lines: impl Iterator<Item = String>, search_for_color: String) -> usize {
+fn part_1<'a>(lines: impl Iterator<Item = &'a String>, search_for_color: &str) -> usize {
     let mapping = read_mapping(lines);
 
     let mut result: HashSet<String> = HashSet::new();
@@ -70,19 +74,19 @@ pub fn doit(lines: impl Iterator<Item = String>, search_for_color: String) -> us
     result.len()
 }
 
-fn count_bags(mapping: &HashMap<String, HashMap<String, usize>>, search_for: String) -> usize {
-    match mapping.get(&search_for) {
+fn count_bags(mapping: &HashMap<String, HashMap<String, usize>>, search_for: &str) -> usize {
+    match mapping.get(search_for) {
         None => 0,
         Some(children) => {
             1 + children
                 .iter()
-                .map(|(color, count)| count * count_bags(mapping, color.clone()))
+                .map(|(color, count)| count * count_bags(mapping, color))
                 .sum::<usize>()
         }
     }
 }
 
-pub fn doit2(lines: impl Iterator<Item = String>, search_for_color: String) -> usize {
+fn part_2<'a>(lines: impl Iterator<Item = &'a String>, search_for_color: &str) -> usize {
     let mapping = read_mapping(lines);
 
     count_bags(&mapping, search_for_color) - 1
@@ -92,8 +96,8 @@ pub fn doit2(lines: impl Iterator<Item = String>, search_for_color: String) -> u
 mod tests {
     use super::*;
 
-    fn test_data() -> impl Iterator<Item = String> {
-        vec![
+    lazy_static! {
+        static ref TEST_DATA: Vec<String> = vec![
             "light red bags contain 1 bright white bag, 2 muted yellow bags.",
             "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
             "bright white bags contain 1 shiny gold bag.",
@@ -106,10 +110,8 @@ mod tests {
         ]
         .into_iter()
         .map(|line| line.to_string())
-    }
-
-    fn test_data2() -> impl Iterator<Item = String> {
-        vec![
+        .collect();
+        static ref TEST_DATA_2: Vec<String> = vec![
             "shiny gold bags contain 2 dark red bags.",
             "dark red bags contain 2 dark orange bags.",
             "dark orange bags contain 2 dark yellow bags.",
@@ -120,20 +122,21 @@ mod tests {
         ]
         .into_iter()
         .map(|line| line.to_string())
+        .collect();
     }
 
     #[test]
-    fn it_works() {
-        assert_eq!(doit(test_data(), "shiny gold".to_string()), 4);
+    fn part_1_works() {
+        assert_eq!(part_1(TEST_DATA.iter(), "shiny gold"), 4);
     }
 
     #[test]
-    fn it_works2() {
-        assert_eq!(doit2(test_data(), "shiny gold".to_string()), 32);
+    fn part_2_works_1() {
+        assert_eq!(part_2(TEST_DATA.iter(), "shiny gold"), 32);
     }
 
     #[test]
-    fn it_works3() {
-        assert_eq!(doit2(test_data2(), "shiny gold".to_string()), 126);
+    fn part_2_works_2() {
+        assert_eq!(part_2(TEST_DATA_2.iter(), "shiny gold"), 126);
     }
 }
