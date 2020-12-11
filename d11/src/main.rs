7b639802b3fbaa5bc435ea44bf67c34036f99c69
Denis Cornehl <denis.cornehl@gmail.com>
2020-12-11 09:23:36 +0100

    [d11] part 2 works

--- d11/src/main.rs
+++ d11/src/main.rs
@@ -12,7 +12,11 @@ fn main() {
         .map(|line| line.unwrap())
         .collect();
 
-    println!("part 1:{:?}", run(&input, 4));
+    println!("part 1: {:?}", run(&input, 4, &count_other_occupied_seats));
+    println!(
+        "part 2: {:?}",
+        run(&input, 5, &count_other_occupied_seats_2)
+    );
 }
 
 fn print_seats(seats: &Vec<Vec<Option<bool>>>) {
@@ -37,6 +41,7 @@ fn read_seats(lines: &Vec<String>) -> Vec<Vec<Option<bool>>> {
             line.chars()
                 .map(|c| match c {
                     'L' => Some(false),
+                    '#' => Some(true),
                     _ => None,
                 })
                 .collect(),
@@ -44,6 +49,46 @@ fn read_seats(lines: &Vec<String>) -> Vec<Vec<Option<bool>>> {
     }
 
     seats
+}
+
+fn count_other_occupied_seats_2(seats: &Vec<Vec<Option<bool>>>, row: usize, col: usize) -> u8 {
+    let directions = vec![
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (-1, 1),
+        (1, -1),
+        (1, 1),
+    ];
+
+    let width: i64 = seats[0].len() as i64;
+    let height: i64 = seats.len() as i64;
+
+    let mut count = 0;
+
+    for (rd, cd) in directions {
+        let mut r = row as i64;
+        let mut c = col as i64;
+        loop {
+            r += rd;
+            c += cd;
+
+            if r < 0 || c < 0 || r >= height || c >= width {
+                break;
+            }
+
+            if let Some(value) = seats[r as usize][c as usize] {
+                if value == true {
+                    count += 1;
+                }
+                break;
+            }
+        }
+    }
+
+    count
 }
 
 fn count_other_occupied_seats(seats: &Vec<Vec<Option<bool>>>, row: usize, col: usize) -> u8 {
@@ -72,7 +117,11 @@ fn count_other_occupied_seats(seats: &Vec<Vec<Option<bool>>>, row: usize, col: u
         .count() as u8
 }
 
-fn run(lines: &Vec<String>, too_many_seats_visible: u8) -> usize {
+fn run(
+    lines: &Vec<String>,
+    too_many_seats_visible: u8,
+    seat_check_function: &dyn Fn(&Vec<Vec<Option<bool>>>, usize, usize) -> u8,
+) -> usize {
     let mut seats = read_seats(&lines);
 
     let mut did_change = true;
@@ -90,7 +139,7 @@ fn run(lines: &Vec<String>, too_many_seats_visible: u8) -> usize {
                     Some(false) => {
                         // If a seat is empty (L) and there are no occupied
                         // seats adjacent to it, the seat becomes occupied.
-                        if count_other_occupied_seats(&seats, row, col) == 0 {
+                        if seat_check_function(&seats, row, col) == 0 {
                             did_change = true;
                             Some(true)
                         } else {
@@ -98,9 +147,9 @@ fn run(lines: &Vec<String>, too_many_seats_visible: u8) -> usize {
                         }
                     }
                     Some(true) => {
-                        // If a seat is occupied (#) and four or more seats
+                        // If a seat is occupied (#) and X our or more seats
                         // adjacent to it are also occupied, the seat becomes empty.
-                        if count_other_occupied_seats(&seats, row, col) >= too_many_seats_visible {
+                        if seat_check_function(&seats, row, col) >= too_many_seats_visible {
                             did_change = true;
                             Some(false)
                         } else {
@@ -140,15 +189,56 @@ mod tests {
         .iter()
         .map(|s| s.to_string())
         .collect();
+        static ref TEST_DATA_SEAT_CHECK_1: Vec<String> = vec![
+            ".......#.",
+            "...#.....",
+            ".#.......",
+            ".........",
+            "..#L....#",
+            "....#....",
+            ".........",
+            "#........",
+            "...#.....",
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
+        static ref TEST_DATA_SEAT_CHECK_2: Vec<String> =
+            vec![".............", ".L.L.#.#.#.#.", ".............",]
+                .iter()
+                .map(|s| s.to_string())
+                .collect();
+        static ref TEST_DATA_SEAT_CHECK_3: Vec<String> =
+            vec![".##.##.", "#.#.#.#", "##...##", "...L...", "##...##", "#.#.#.#", ".##.##.",]
+                .iter()
+                .map(|s| s.to_string())
+                .collect();
     }
 
     #[test]
     fn part_1_works() {
-        assert_eq!(run(&TEST_DATA, 4), 37);
+        assert_eq!(run(&TEST_DATA, 4, &count_other_occupied_seats), 37);
     }
 
-    // #[test]
-    // fn part_2_works() {
-    //     assert_eq!(run2(&TEST_DATA), 26);
-    // }
+    #[test]
+    fn new_seat_check() {
+        let seats = read_seats(&TEST_DATA_SEAT_CHECK_1);
+        assert_eq!(count_other_occupied_seats_2(&seats, 4, 3), 8);
+    }
+
+    #[test]
+    fn new_seat_check_2() {
+        let seats = read_seats(&TEST_DATA_SEAT_CHECK_2);
+        assert_eq!(count_other_occupied_seats_2(&seats, 1, 1), 0);
+    }
+    #[test]
+    fn new_seat_check_3() {
+        let seats = read_seats(&TEST_DATA_SEAT_CHECK_3);
+        assert_eq!(count_other_occupied_seats_2(&seats, 3, 3), 0);
+    }
+
+    #[test]
+    fn part_2_works() {
+        assert_eq!(run(&TEST_DATA, 5, &count_other_occupied_seats_2), 26);
+    }
 }
