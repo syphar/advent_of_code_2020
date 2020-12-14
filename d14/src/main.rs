ee6d6b4f65bd70b2086669f457d77bddc4537538
Denis Cornehl <denis.cornehl@gmail.com>
2020-12-14 20:28:24 +0100

    [d14] move parsing into separate module Enum and FromStr

--- d14/src/main.rs
+++ d14/src/main.rs
@@ -1,7 +1,6 @@
-#[macro_use]
-extern crate lazy_static;
+mod commands;
+use commands::Command;
 
-use regex::Regex;
 use simple_error::SimpleError;
 use std::collections::HashMap;
 use std::fs::File;
@@ -10,73 +9,51 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let file = File::open("input.txt").unwrap();
 
-    let input: Vec<String> = BufReader::new(file)
+    let input: Vec<Command> = BufReader::new(file)
         .lines()
-        .map(|line| line.unwrap())
+        .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
     println!("part 1: {:?}", part_1(input.iter()));
 }
 
-lazy_static! {
-    static ref REGEX_MASK: Regex = Regex::new(r"mask = (\w{36})").unwrap();
-    static ref REGEX_MEM: Regex = Regex::new(r"mem\[(\d+)\] = (\d+)").unwrap();
-}
-
-fn part_1<'a>(lines: impl Iterator<Item = &'a String>) -> Result<u64, SimpleError> {
-    let mut memory: HashMap<usize, u64> = HashMap::new();
+fn part_1<'a>(commands: impl Iterator<Item = &'a Command>) -> Result<u64, SimpleError> {
+    let mut memory: HashMap<u64, u64> = HashMap::new();
 
     let mut current_mask: HashMap<u8, u8> = HashMap::new();
 
-    for line in lines {
-        println!("\nline {:?}", line);
+    for command in commands {
+        match command {
+            Command::Mask(mask) => {
+                current_mask = mask
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, v)| v.is_some())
+                    .map(|(i, v)| (i as u8, if v.unwrap() == true { 1 } else { 0 }))
+                    .collect();
+            }
+            Command::Set(adr, v) => {
+                let mut value = *v;
 
-        if let Some(cap) = REGEX_MASK.captures(line) {
-            let mask = cap.get(1).unwrap().as_str();
-
-            current_mask = mask
-                .chars()
-                .rev()
-                .enumerate()
-                .filter(|(_, v)| *v != 'X')
-                .map(|(i, v)| (i as u8, if v == '1' { 1 } else { 0 }))
-                .collect();
-
-            println!("\tis mask: {}", mask);
-            println!("\t{:?}", current_mask);
-        } else if let Some(cap) = REGEX_MEM.captures(line) {
-            let adr: usize = cap.get(1).unwrap().as_str().parse().unwrap();
-            let mut value: u64 = cap.get(2).unwrap().as_str().parse().unwrap();
-
-            println!("\tis mem: {} = {}", adr, value);
-
-            println!("\tbits before: {:#036b} (decimal {})", value, value);
-
-            for (bit, bitvalue) in current_mask.iter() {
-                match *bitvalue {
-                    1 => {
-                        // set a bit
-                        let mask = 1 << bit;
-                        println!("\t\t set bit {}, mask {:#036b}", bit, mask);
-                        value |= mask;
-                    }
-                    0 => {
-                        // remove a bit
-                        let mask = !(1 << bit);
-                        println!("\t\t remove bit {}, mask {:#036b}", bit, mask);
-                        value &= mask;
-                    }
-                    _ => {
-                        return Err(SimpleError::new("unknown bit value"));
+                for (bit, bitvalue) in current_mask.iter() {
+                    match *bitvalue {
+                        1 => {
+                            // set a bit
+                            let mask = 1 << bit;
+                            value |= mask;
+                        }
+                        0 => {
+                            // remove a bit
+                            let mask = !(1 << bit);
+                            value &= mask;
+                        }
+                        _ => {
+                            return Err(SimpleError::new("unknown bit value"));
+                        }
                     }
                 }
+                memory.insert(*adr, value);
             }
-
-            println!("\tbits after: {:#036b} (decimal {})", value, value);
-
-            memory.insert(adr, value);
-        } else {
-            return Err(SimpleError::new(format!("unknown line: {}", line)));
         }
     }
 
@@ -86,16 +63,17 @@ fn part_1<'a>(lines: impl Iterator<Item = &'a String>) -> Result<u64, SimpleErro
 #[cfg(test)]
 mod tests {
     use super::*;
+    use lazy_static::lazy_static;
 
     lazy_static! {
-        static ref TEST_DATA: Vec<String> = vec![
+        static ref TEST_DATA: Vec<Command> = vec![
             "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
             "mem[8] = 11",
             "mem[7] = 101",
             "mem[8] = 0",
         ]
         .into_iter()
-        .map(|line| line.to_string())
+        .map(|line| line.to_string().parse().unwrap())
         .collect();
     }
 
